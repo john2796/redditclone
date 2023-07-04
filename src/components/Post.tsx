2969947d66f1650a78ac1fd585daa2641c297f58
john2796 <jbmiranda22796@gmail.com>
2023-07-04 14:52:07 -0700

    Adding api route

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,6 +1,7 @@
 import { formatTimeToNow } from '@/lib/utils'
 import { Post, Vote, User } from '@prisma/client'
 import { FC, useRef } from 'react'
+import PostVoteClient from './PostVoteClient'
 
 type PartialVote = Pick<Vote, 'type'>
 
@@ -25,22 +26,35 @@ const Post: FC<PostProps> = ({
   const pRef = useRef<HTMLParagraphElement>(null)
 
   return (
-    <div>
-      <div>
-        {/* PostVoteClient */}
+    <div className="rounded-md bg-white shadow">
+      <div className="px-6 py-4 flex justify-between">
+        <PostVoteClient
+          postId={post.id}
+          initialVotesAmt={_votesAmt}
+          initialVote={_currentVote?.type}
+        />
 
-        <div>
-          <div>
+        <div className="w-0 flex-1">
+          <div className="max-h-40 mt-1 text-xs text-gray-500">
             {subredditName && (
               <>
-                <a>
-                  <span className="px-1">•</span>
+                <a
+                  className="underline text-zinc-900 text-sm underline-offset-2"
+                  href={`/r/${subredditName}`}
+                >
+                  r/{subredditName}
                 </a>
+                <span className="px-1">•</span>
               </>
             )}
             <span>Posted by u/{post.author.username}</span>
             {formatTimeToNow(new Date(post.createdAt))}
           </div>
+          <a href={`/r/${subredditName}/post/${post.id}`}>
+            <h1 className="text-lg font-semibold py-2 leading-6 text-gray-900">
+              {post.title}
+            </h1>
+          </a>
         </div>
       </div>
     </div>
